cd842f09e917004dd3f4c58c05ee8ab4f2fe6574
Oschdi <osterjakob@gmail.com>
2026-03-14 16:13:31 +0100

    Phase function

--- shaders/common/math.glsl
+++ shaders/common/math.glsl
@@ -1,4 +1,8 @@
+#ifndef MATH_GLSL
+#define MATH_GLSL
+
 #define PI 3.14159265
+#define INV_4_PI 0.07957747
 
 float safeSqrt(float x) {
     return sqrt(max(0, x));
@@ -65,3 +69,32 @@ vec3 faceForward(vec3 n, vec3 v) {
 float lerp(float x, float a, float b) {
     return (1 - x) * a + x * b;
 }
+
+struct Frame {
+    vec3 x, y, z;
+};
+
+void coordinateSystem(vec3 v1, inout vec3 v2, inout vec3 v3) {
+    float sign = sign(v1.z);
+    float a = -1.0 / (sign + v1.z);
+    float b = v1.x * v1.y * a;
+    v2 = vec3(1.0 + sign * sqr(v1.x) * a, sign * b, -sign * v1.x);
+    v3 = vec3(b, sign + sqr(v1.y) * a, -v1.y);
+}
+
+Frame frameFromZ(vec3 z) {
+    vec3 x = vec3(0);
+    vec3 y = vec3(0);
+    coordinateSystem(z, x, y);
+    return Frame(x, y, z);
+}
+
+vec3 fromLocal(vec3 v, Frame frame) {
+    return v.x * frame.x + v.y * frame.y + v.z * frame.z;
+}
+
+vec3 sphericalDirection(float sinTheta, float cosTheta, float phi) {
+    return vec3(clamp(sinTheta, -1.0, 1.0) * cos(phi), clamp(sinTheta, -1.0, 1.0) * sin(phi), clamp(cosTheta, -1, 1));
+}
+
+#endif
