ed17befd393da2e1941c6e5454300e1cbed6befd
Oschdi <osterjakob@gmail.com>
2026-06-03 19:51:43 +0200

    Option Setup

--- include/engine/rendering/resources/materials/MetalRoughMaterial.hpp
+++ include/engine/rendering/resources/materials/MetalRoughMaterial.hpp
@@ -32,6 +32,7 @@ namespace RtEngine {
 	private:
 
 		bool normal_mapping = false, sample_lights = false, sample_bsdf = false, russian_roulette = false;
+		float similarity_relation_factor = 1.0f;
 
 		VkSampler sampler;
 	};
