9e0b1fd4dac0efd4e085f315f059ab35df7f1f1e
Oschdi <osterjakob@gmail.com>
2026-06-18 13:57:08 +0200

    Error scale fix

--- scripts/generate_plots.R
+++ scripts/generate_plots.R
@@ -7,17 +7,16 @@ output_path <- args[2]
 
 data <- read.csv(data_path)
 
-# ensure expected columns
 names(data) <- c("name", "samples", "mse")
 
 pdf(output_path, width = 10, height = 7)
 
 data %>%
     group_by(name) %>%
-    ggplot(aes(x = log2(samples), y = log2(mse), color = name)) +
+    ggplot(aes(x = log2(samples), y = mse, color = name)) +
     geom_line() +
     scale_x_continuous("Samples", labels = math_format(2^.x)) +
-    scale_y_continuous("Log MSE") +
+    scale_y_continuous("MSE", trans = "log2", labels = label_number()) +
     labs(title = "Log MSE against reference image", color = "Name")
 
 dev.off()
